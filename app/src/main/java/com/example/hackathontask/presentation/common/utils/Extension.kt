b37fd7643934cc30957ae95f6ae21b96a20c276f
ruhlandev <r.usubov@expressbank.az>
2024-06-29 06:59:01 +0400

    added terms and condition dialog extension

---
+++ app/src/main/java/com/example/hackathontask/presentation/common/utils/Extension.kt
@@ -0,0 +1,34 @@
+package com.example.hackathontask.presentation.common.utils
+
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.hackathontask.databinding.TermsDialogBinding
+import com.example.hackathontask.presentation.common.base.setSafeOnClickListener
+
+fun Context.createTermsAndConditionDialog(
+    onConfirmOnClick: () -> Unit
+): Dialog {
+    val dialog = Dialog(this, androidx.appcompat.R.style.Theme_AppCompat_Dialog)
+    val binding = TermsDialogBinding.inflate(LayoutInflater.from(this))
+
+    dialog.setCancelable(false)
+    dialog.setContentView(binding.root)
+    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent);
+    dialog.window?.setLayout(
+        ConstraintLayout.LayoutParams.MATCH_PARENT,
+        ConstraintLayout.LayoutParams.MATCH_PARENT
+    )
+
+    with(binding) {
+        buttonCancel.setSafeOnClickListener {
+            dialog.cancel()
+        }
+        buttonConfirm.setSafeOnClickListener {
+            onConfirmOnClick()
+        }
+    }
+
+    return dialog
+}
