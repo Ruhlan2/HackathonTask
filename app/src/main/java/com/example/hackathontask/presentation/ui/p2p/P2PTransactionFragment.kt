149487eca9eaad46666cda380bc66c238aa258f8
ruhlandev <r.usubov@expressbank.az>
2024-06-29 12:55:50 +0400

    incompleted feature

---
+++ app/src/main/java/com/example/hackathontask/presentation/ui/p2p/P2PTransactionFragment.kt
@@ -0,0 +1,13 @@
+import com.example.hackathontask.databinding.FragmentP2PTransactionBinding
+import com.example.hackathontask.presentation.common.base.BaseFragment
+
+class P2PTransactionFragment :BaseFragment<FragmentP2PTransactionBinding>(FragmentP2PTransactionBinding::inflate) {
+    override fun onViewCreateFinish() {
+
+    }
+
+    override fun observeEvents() {
+
+    }
+
+}
